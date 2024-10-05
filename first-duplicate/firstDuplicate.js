c994e37ec0c4790e0a5bac6ea48ee8a1aac178ea
Jay <ihearthuskiez@gmail.com>
2024-10-05 13:50:39 -0600

    first question done

--- first-duplicate/firstDuplicate.js
+++ first-duplicate/firstDuplicate.js
@@ -1,3 +1,12 @@
 function firstDuplicate(array) {
-  
+  for (let i = 0; i < array.length; i++) {
+    let num1 = array[i];
+    for (let j = 0; j < i; j++) {
+        let num2 = array[j];
+        if (num1 === num2) {
+            return num1;
+        }
+    }
+  }
+  return -1;
 }
