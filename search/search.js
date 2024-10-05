1767a280ea39d598521c3ba291aae1bdabd2836c
Jay <ihearthuskiez@gmail.com>
2024-10-05 14:26:29 -0600

    question two finished

--- search/search.js
+++ search/search.js
@@ -1,3 +1,10 @@
 function search(nameList, searchTerm) {
+    let returnString = [];
 
+    for (let i = 0; i < nameList.length; i++) {
+        if (nameList[i].toLowerCase().includes(searchTerm.toLowerCase()) === true) {
+            returnString.push(nameList[i]);
+        }
+    }
+    return returnString;
 }
